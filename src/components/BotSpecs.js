48a81d077ec873a5621ebbf34520a42cf36d9401
Simion <simion.okola@student.moringaschool.com>
2022-10-03 10:14:36 +0300

    ft-BotSpecs

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -9,17 +9,13 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-function BotSpecs({ bot }) {
+function BotSpecs({ bot, handleDisplay, deleteBot}) {
   return (
     <div className="ui segment">
       <div className="ui two column centered grid">
         <div className="row">
           <div className="four wide column">
-            <img
-              alt="oh no!"
-              className="ui medium circular image bordered"
-              src={bot.avatar_url}
-            />
+            <img alt="oh no!" className="ui medium circular image bordered" src={bot.avatar_url} />
           </div>
           <div className="four wide column">
             <h2>Name: {bot.name}</h2>
@@ -50,22 +46,10 @@ function BotSpecs({ bot }) {
                 </div>
               </div>
             </div>
-            <button
-              className="ui button fluid"
-              onClick={() =>
-                console.log("connect this to a function that shows all bots")
-              }
-            >
+            <button className="ui button fluid" onClick={() => handleDisplay}>
               Go Back
             </button>
-            <button
-              className="ui button fluid"
-              onClick={() =>
-                console.log(
-                  "connect this to a function that adds this bot to your bot army list"
-                )
-              }
-            >
+            <button className="ui button fluid" onClick={() => handleDisplay}>
               Enlist
             </button>
           </div>
