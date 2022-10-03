47b6f103704f44ff28b718e2ae8166b8904a4b6f
Simion <simion.okola@student.moringaschool.com>
2022-10-03 09:50:51 +0300

    ft-BotCollection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,15 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function BotCollection() {
+function BotCollection({ BotArr, handleDisplay, DeleteBot }) {
   // Your code here
   return (
     <div className="ui four column grid">
+      {"Bot Collection click on a bot to add it to your army."}
       <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+      {BotArr.map((bot) => (
+            <BotCard key={bot.id} bot={bot} handleDisplay={handleDisplay} DeleteBot={DeleteBot} />
+        ))}
       </div>
     </div>
   );
