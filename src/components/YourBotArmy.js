32aedb327fdc72ef54844350506a259f442c4ba8
Simion <simion.okola@student.moringaschool.com>
2022-10-03 09:45:57 +0300

    ft-BotArmy

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,14 +1,17 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
+function YourBotArmy({ BotArr, handleDisplay, DeleteBot }) {
   //your bot army code here...
 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
+        {"Click on a Bot on the bot collection to enlist it to your army."}
         <div className="row bot-army-row">
-          {/*...and here...*/}
-          Your Bot Army
+          {BotArr.map((bot) => (
+            <BotCard key={bot.id} bot={bot} handleDisplay={handleDisplay} DeleteBot={DeleteBot} />
+          ))}
         </div>
       </div>
     </div>
